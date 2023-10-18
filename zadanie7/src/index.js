0924df3a1111de97939605cb3279af6742a0a365
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-18 18:37:10 +0200

    fix: zadanie 7

--- zadanie7/src/index.js
+++ zadanie7/src/index.js
@@ -2,8 +2,16 @@ const answer = (users) => {
 	return users.reduce((acc, elem) => {
 		acc.push({
 				[elem.name]: elem.likes,
-				age: elem.name * 10
+				age: elem.name.length * 10
 			});
 		return acc;
 	}, []);
 };
+
+const myUsers = [
+    { name: "shark", likes: "ocean" },
+    { name: "turtle", likes: "pond" },
+    { name: "otter", likes: "fish biscuits" }
+]
+
+console.log(answer(myUsers));
