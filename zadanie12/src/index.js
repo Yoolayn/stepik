ae3c72388097a54bc3228354005af3b831390632
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-25 07:03:41 +0200

    feat: zadanie 12

--- zadanie12/src/index.js
+++ zadanie12/src/index.js
@@ -1,10 +1,11 @@
 function asyncDivide(num1, num2) {
-	setTimeout(() => {
-		if (num2 === 0) {
-			console.log("Nie można dzielić przez zero");
-		} else {
-			console.log(num1 / num2);
-		}
-	}, 1000)
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if (num2 !== 0) {
+				resolve(num1 / num2);
+			} else {
+				reject("Nie można dzielić przez zero");
+			}
+		}, 1000);
+	});
 }
-
