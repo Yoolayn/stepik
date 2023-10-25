cf29c02c019aa13081d47d995d7b9ead174cc7d1
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-25 09:08:47 +0200

    fix: zadanie 11

--- zadanie11/src/index.js
+++ zadanie11/src/index.js
@@ -1,10 +1,10 @@
-function printNumbers () {
-	const range = Array.from({length: 5}, (_, i) => i + 1)
-	range.map((elem) => {
-		setTimeout(() => {
-			console.log(elem);
-		}, 10)
-	})
+const printNumber = async () => {
+	for (let i = 1; i <= 5; i++) {
+		await new Promise((resolve) =>
+			setTimeout(() => {
+				console.log(i);
+				resolve();
+			}, 1000)
+		);
+	}
 }
-
-printNumbers()
